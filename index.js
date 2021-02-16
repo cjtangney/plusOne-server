89e9d9aa10bf48d90902f0878fae5cbb172ab067
cjtangney <connor.j.tangney@gmail.com>
2021-02-15 21:29:25 -0500

    Trying more CORS stuf

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-var allowlist = ['https://plus-one.vercel.app/'];
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+var allowlist = ['http://localhost:3333', 'https://plus-one.vercel.app/'];
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 
-app.use(cors(corsOptionsDelegate));
+// app.use(cors(corsOptionsDelegate));
+app.use(cors('https://plus-one.vercel.app/'));
 
 const mongo = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;

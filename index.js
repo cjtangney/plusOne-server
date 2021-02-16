1ebe40a046acb910e99ed601cf150e03b095e3ec
cjtangney <connor.j.tangney@gmail.com>
2021-02-15 21:16:17 -0500

    Update CORS policy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-var allowlist = ['http://localhost:3333', 'https://plus-one.vercel.app/'];
+var allowlist = ['https://plus-one.vercel.app/'];
 var corsOptionsDelegate = function (req, callback) {
   var corsOptions;
   if (allowlist.indexOf(req.header('Origin')) !== -1) {

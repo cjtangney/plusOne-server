e10db789ef147f2e759e061eb48fd52d77740f2a
cjtangney <connor.j.tangney@gmail.com>
2021-02-15 21:14:39 -0500

    Update CORS policy

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({
-  origin: ['http://localhost:3333', 'https://plus-one.vercel.app/'],
-  methods: 'GET',
-}));
+var allowlist = ['http://localhost:3333', 'https://plus-one.vercel.app/'];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use(cors(corsOptionsDelegate));
 
 const mongo = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;

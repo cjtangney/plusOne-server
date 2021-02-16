8f3bada956df2ef8730c79c30c1ab7d6c4941a34
cjtangney <connor.j.tangney@gmail.com>
2021-02-15 21:31:36 -0500

    Last CORS test for the night

--- index.js
+++ index.js
@@ -2,19 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-var allowlist = ['http://localhost:3333', 'https://plus-one.vercel.app/'];
-// var corsOptionsDelegate = function (req, callback) {
-//   var corsOptions;
-//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-//   } else {
-//     corsOptions = { origin: false } // disable CORS for this request
-//   }
-//   callback(null, corsOptions) // callback expects two parameters: error and options
-// }
-
-// app.use(cors(corsOptionsDelegate));
-app.use(cors('https://plus-one.vercel.app/'));
+app.use(cors({
+  origin: 'https://plus-one-server.vercel.app',
+}));
 
 const mongo = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;

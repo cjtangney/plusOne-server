b04b00fd822e9dfcd8fb3f11abce7bf47150c59b
cjtangney <connor.j.tangney@gmail.com>
2021-02-15 21:05:39 -0500

    basic express server to serve heroes

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3333', 'https://plus-one.vercel.app/'],
+  methods: 'GET',
+}));
+
+const mongo = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const { resolve } = require('path');
+const { homedir } = require('os');
+const url = 'mongodb+srv://root:cU2P8myQ2y332Di@wr-winrate.zn8ks.mongodb.net/winrates-WR?retryWrites=true&w=majority';
+const dbName = 'list_01';
+const collectionName = 'heroes';
+
+app.get('/heroes', async (_, res) => {
+  const client = new mongo(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  client.connect().then(() => {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    return cursor = collection.find({});
+  }).then((cursor) => {
+    return cursor.toArray()
+  }).then((data) => {
+    res.send(data);
+    client.close();
+  });
+});
+
+app.post('/update\::hero', (req, res) => {
+  console.log('updating: ', req.params.hero);
+  const hero = JSON.parse(req.params.hero);
+  const o_ID = new ObjectID(hero._id);
+  hero._id = o_ID;
+
+  const client = new mongo(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  client.connect().then(async () => {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const filter = { _id: o_ID };
+    const result = await collection.updateOne(filter, { $set: hero });
+    return(result);
+  }).then((info) => {
+    console.log(info.result);
+    client.close();
+    res.send();
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running... 🚀', '\n', 'port: 3000');
+});
